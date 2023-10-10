5df80123f692c051836e57692ad9f16e94813842
Hala Almohammadi <hala.almohammadi@kcl.ac.uk>
2023-10-10 15:11:44 +0100

    fix max_length varibl

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 #from django.contrib.auth.models import AbstractUser
 
 class Thing(models.Model):
-    name = models.CharField(max=50)
+    name = models.CharField(max_length=50)
     description = models.TextField()
     quantity = models.IntegerField()
